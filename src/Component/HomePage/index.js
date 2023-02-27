d19552b25fd9203d38a98d1e66d29e6f377ae5bc
Aksh Darji <akshdarji2019@gmail.com>
2023-02-27 21:03:35 +0530

    GoogleMap component is add

--- src/Component/HomePage/index.js
+++ src/Component/HomePage/index.js
@@ -8,7 +8,7 @@ import Navbar from '../Navbar';
 import './Home.css'
 import { useHistory } from "react-router-dom";
 import FeedbackForm from '../FeedbackForm/FeedbackForm';
-
+import GoogleMap from '../GoogleMap/GoogleMap';
 export const Header = () => {
 
 	const header_height = useRef(null);
@@ -112,6 +112,7 @@ export const Info = () => {
 				<div>
 					<ShowRequestPost />
 				</div>
+				 
 			</div>
 			<hr />
 			<h2 align="center">Our Diamonds</h2>
@@ -244,7 +245,9 @@ export const Info = () => {
 	</div>
 
 			<Faq />
+			<GoogleMap from={{lat: 23.596952669460926, lng: 72.38002810670397}} to={{lat: 23.23782062216229, lng: 72.54850214451702}}/>
 			<FeedbackForm/>
+			
 			<div id='contact'>{Contact()}</div>
 		</>
 	);
